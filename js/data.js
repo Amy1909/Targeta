6303b5c5d62ac577f4afdba3baa8663094800495
Amy Lourdes Ortega De León <79715201+Amy1909@users.noreply.github.com>
2022-08-04 23:15:21 -0600

    Add files via upload
    funcion

---
+++ js/data.js
@@ -0,0 +1,5 @@
+//DATA
+export let target1 = ["🍇", "🍒", "🥝", "🍉", "🫐", "🍎", "🍐", "🍓","🍈"]
+export let target2 = ["🍇", "🍒", "🥝", "🍉", "🫐", "🍎", "🍐", "🍓","🍈"]
+
+export let complete_target= target1.concat(target2);
