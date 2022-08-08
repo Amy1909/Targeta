bf68e05214c790f8e4a14083e7a9428e9ef990ad
Amy Lourdes Ortega De León <79715201+Amy1909@users.noreply.github.com>
2022-08-07 20:18:02 -0600

    Delete js directory

--- js/data.js
+++
@@ -1,5 +0,0 @@
-//DATA
-export let target1 = ["🍇", "🍒", "🥝", "🍉", "🫐", "🍎", "🍐", "🍓","🍈"]
-export let target2 = ["🍇", "🍒", "🥝", "🍉", "🫐", "🍎", "🍐", "🍓","🍈"]
-
-export let complete_target= target1.concat(target2);
